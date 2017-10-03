a3d4f46090e87d373d95510662197f4b8404b894
joshtheclasher <32440072+joshtheclasher@users.noreply.github.com>
2017-10-02 20:39:08 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
     $("h1").slideDown("slow");
+    $("h1").mouseenter(function(){
+        $("h1").css("color: blue;");
+    }).mouseleave(function(){
+        $("h1").css("color: black;");
+    });
     $("p").slideDown("slow");
 });
